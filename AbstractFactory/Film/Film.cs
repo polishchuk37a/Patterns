9bcaeeb8920971be70279773b319bcf74ff3f769
polishchuk37a <polishchuk.andrey2002@gmail.com>
2021-02-20 17:24:27 +0200

    Remade class Film

---
+++ AbstractFactory/Film/Film.cs
@@ -0,0 +1,34 @@
+﻿using AbstractFactoryFilms.factories;
+using AbstractFactoryFilms.filmStrip.soundtrack;
+using AbstractFactoryFilms.filmStrip.subtitle;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactoryFilms
+{
+    public class Film
+    {
+        private ISound sound;
+        private ISubtitle subtitle;
+
+        public Film(IFactory factory)
+        {
+            if (factory != null)
+            {
+                sound = factory.Sound();
+                subtitle = factory.Subtitle();
+            }
+        }
+
+        public void play()
+        {
+            if (sound != null && subtitle != null)
+            {
+
+                Console.WriteLine(sound.Play());
+                Console.WriteLine(subtitle.Play());
+            }
+        }
+    }
+}
--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -6,10 +6,54 @@ namespace AbstractFactoryFilms
 {
     class Program
     {
+        private static int choice = 0;
         static void Main(string[] args)
         {
-            new Client().Main();
+            do
+            {
+                Film film = GetFilm();
+                if (film != null)
+                {               
+                    film.play();
+                }
+                Console.WriteLine();
+            } while (choice != 4);
         }
-      
+
+        public static Film GetFilm()
+        {        
+            IFactory factory = null;
+
+            do
+            {
+                Console.Write("Select language: \n 1-Russian \n 2-English \n 3-Ukrainian \n 4-Exit \n Your choice is: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            factory = new RussianFactory();
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 2:
+                        {
+                            factory = new EnglishFactory();
+                            Console.WriteLine();
+                            break;
+                        }
+                    case 3:
+                        {
+                            factory = new UkrainianFactory();
+                            Console.WriteLine();
+                            break;
+                        }
+                }
+
+            } while (choice <= 0 || choice > 4);
+            return new Film(factory);
+        }
     }
 }
+
